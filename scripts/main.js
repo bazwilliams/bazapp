c1b7c18593f95fb05aee98de090b8835d988b9e7
Barry Williams <barry.williams@linn.co.uk>
2012-12-06 15:22:19 +0000

    Added underscore and handlebars to requirejs shim

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
 require.config({
-	paths: {
-		jquery: 'jquery-1.8.2.min',
-		underscore: 'underscore-min',
-		handlebars: 'handlebars-1.0.rc.1',
-		backbone: 'backbone-min'
-	},
+    paths: {
+        jquery: 'jquery-1.8.2.min',
+        underscore: 'underscore-min',
+        handlebars: 'handlebars-1.0.rc.1',
+        backbone: 'backbone-min'
+    },
 
-	shim: {
-		"backbone": {
-			deps: ["underscore", "jquery"],
-			exports: "Backbone"
-		}
-	}
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        }
+    }
 });
 
-require([
-	'app',
-], function(App){
+require(['app'], function(App) {
 	App.initialize();
 });
